601d92b3045362424c4880e566c2845a083e0f65
Paul van Mierlo <pauljozefvanmierlo@yahoo.com>
2020-10-30 08:36:27 +0100

    wijzigingen

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 function addMoviesToDom(filmSelectie) {
-    let moviesPosters = filmSelectie.map(element => element.Poster);
-    let moviesTitles = filmSelectie.map(element => element.Title);
-    let ids = filmSelectie.map(element => element.imdbID);
     let filmLijst = document.getElementById("filmLijst");
-    filmLijst.innerHTML = "";
+    filmLijst.innerHTML = ""; /* eenvoudigste manier om de lijst leeg te maken */
     let basisref = "https://www.imdb.com/title/";
-    let lis = filmSelectie.map(element => {
-        let refDeelOpenen = '<a href="' + basisref + element.imdbID + '">';
-        let plaatjesdeel = '<img src="' + element.Poster + '" width="100% alt="' + element.Title + '"/>';
-        return '<li>' + refDeelOpenen + plaatjesdeel + '</a></li>'
-    });
-    lis.forEach(element => filmLijst.innerHTML += element);
-    let plaatjes = document.getElementsByTagName("img");
-    let plaatjesArr = Array.from(plaatjes);
-    plaatjesArr.forEach(element => element.classList.add("plaatje"));
+
+    filmSelectie.forEach(film => {
+        /* de onderstaand code is wellicht niet gesuggereerd, maar ik vind het wel het meest leesbaar */
+        let poster = document.createElement('img');
+        poster.setAttribute("src", film.Poster);
+        poster.setAttribute("alt", film.title);
+        poster.setAttribute("width", "100%");
+        poster.classList.add("poster");
+        let aref = document.createElement("a");
+        aref.setAttribute("href", basisref + film.imdbID);
+        aref.appendChild(poster);
+        let ulListItem = document.createElement("li");
+        ulListItem.appendChild(aref);
+        filmLijst.appendChild(ulListItem);
+    })
 }
 
 function handleOnChangeEvent(e) {
@@ -46,7 +49,6 @@ function checkFilmInhoud() {
 }
 
 function zoekFilmTitels(e) {
-    console.log("zoek op titel");
     if (filmZoekTerm.value == "") {
         alert("Voor deze optie moet u een of meerdere woorden voor de titel invullen")
     } else {
@@ -55,9 +57,11 @@ function zoekFilmTitels(e) {
             alert("De zoekterm voor de film moet minimaal 5 karakters bevatten");
         else {
             films = movies.filter(film => film.Title.toUpperCase().includes(filmZoekTerm.value.toUpperCase()));
-            console.log("aantal films : " + films.length);
             selectieKeuzeArr.forEach(element => { if (element.checked === true) element.checked = false; });
-            addMoviesToDom(films);
+            if (films.length === 0) {
+                alert("Op basis van de door u opgegeven zoekstrings zijn geen films gevonden");
+                filmZoekTerm.value = "";
+            } else { addMoviesToDom(films); }
         }
     }
 }
@@ -68,5 +72,7 @@ let selectieKeuze = document.getElementsByClassName("radio");
 let selectieKeuzeArr = Array.from(selectieKeuze);
 let zoekFilm = document.getElementById("zoek-film-titel");
 let filmZoekTerm = document.getElementById("film-titel");
+/*let body = document.querySelector("body");
+body.onload = checkFilmInhoud();*/
 zoekFilm.addEventListener("click", zoekFilmTitels);
 selectieKeuzeArr.forEach(element => element.addEventListener('change', handleOnChangeEvent));
